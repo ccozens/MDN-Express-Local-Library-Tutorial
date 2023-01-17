820488672e5509a79a49e58dfdb33f864ad0c530
ccozens <ccozens@gmail.com>
2023-01-17 12:40:59 +0000

    added node version to package.json and updated mongoDB in app.js for deployment

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const app = express();
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
-const mongoDB = "mongodb+srv://christutorial:OrOW5YTHKjtRM3NK@cluster0.uw3jqpl.mongodb.net/local_library?retryWrites=true&w=majority";
+const dev_db_url = "mongodb+srv://christutorial:OrOW5YTHKjtRM3NK@cluster0.uw3jqpl.mongodb.net/local_library?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
